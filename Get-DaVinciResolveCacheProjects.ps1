98ac2c6b14f2963b86ec2546dc0da785749c892d
Christian Rondeau <christian.rondeau@gmail.com>
2017-03-15 12:33:09 -0400

    Show project size

--- Get-DaVinciResolveCacheProjects.ps1
+++ Get-DaVinciResolveCacheProjects.ps1
@@ -14,6 +14,16 @@ Param(
 Set-StrictMode -version Latest
 $ErrorActionPreference = "Stop"
 
+Function Format-FileSize() {
+    Param ([int]$Size)
+    If     ($Size -gt 1TB) {[string]::Format("{0:0.00} TB", $Size / 1TB)}
+    ElseIf ($Size -gt 1GB) {[string]::Format("{0:0.00} GB", $Size / 1GB)}
+    ElseIf ($Size -gt 1MB) {[string]::Format("{0:0.00} MB", $Size / 1MB)}
+    ElseIf ($Size -gt 1KB) {[string]::Format("{0:0.00} kB", $Size / 1KB)}
+    ElseIf ($Size -gt 0)   {[string]::Format("{0:0.00} B", $Size)}
+    Else                   {"$Size"}
+}
+
 $CachePath = Resolve-Path $Cache
 
 If (-Not (Test-Path $CachePath)) {
@@ -23,5 +33,6 @@ If (-Not (Test-Path $CachePath)) {
 $CacheProjects = Get-ChildItem $CachePath -Directory
 
 ForEach ($CacheProject in $CacheProjects) {
-	Write-Output "Project: $CacheProject"
+	$CacheProjectSize = Get-ChildItem | Measure-Object -Sum Length | Select-Object Sum
+	Write-Output "Project: $CacheProject = $(Format-FileSize $CacheProjectSize.Sum)"
 }
